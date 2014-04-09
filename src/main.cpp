c08fe45910b8e7f1c885c3085f12edf5078abb8b
Justin Bisignano <justinbis@fake.server>
2014-04-09 13:43:25 -0400

    Reorganized Source
    Inc folder now gone

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  COP-3503-Project
+//
+//  Created by Justin on 4/9/14.
+//
+//
+
+#include "main.h"
+
+int main(int argc, const char * argv[])
+{
+	// TODO: Everything
+}
+
+/**
+ * Prints a menu to stdout
+ */
+void printMenu()
+{
+	//TODO: Whatever
+}
