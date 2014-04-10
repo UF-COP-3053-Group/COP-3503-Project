33bff45a70df986c85728c22fb55aaa99e196c35
Tylor Childers <tylord1@gmail.com>
2014-04-10 18:35:37 -0400

    Added add subtract multiply and divide methods to each *.h file except Number.h

--- src/Log.h
+++ src/Log.h
@@ -21,6 +21,37 @@ public:
 	Log();
 	
 	// TODO: need to implement add, subtract, etc. How do we implement this dependent upon Number subclass?
+	void add(Number);
+	void add(Constant);
+	void add(Exponent);
+	void add(Integer);
+	void add(Log);
+	void add(Radical);
+	void add(Rational);
+
+	void subtract(Number);
+	void subtract(Constant);
+	void subtract(Exponent);
+	void subtract(Integer);
+	void subtract(Log);
+	void subtract(Radical);
+	void subtract(Rational);
+
+	void multiply(Number);
+	void multiply(Constant);
+	void multiply(Exponent);
+	void multiply(Integer);
+	void multiply(Log);
+	void multiply(Radical);
+	void multiply(Rational);
+
+	void divide(Number);
+	void divide(Constant);
+	void divide(Exponent);
+	void divide(Integer);
+	void divide(Log);
+	void divide(Radical);
+	void divide(Rational);
 };
 
 #endif /* defined(__COP_3503_Project__Log__) */
