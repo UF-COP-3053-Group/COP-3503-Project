63eca1e2985b2c988a765bc3e3d574aa61b52fd0
Hanif <hanif314@gmail.com>
2014-04-14 01:44:26 -0400

    Integer class

--- src/Integer.cpp
+++ src/Integer.cpp
@@ -26,3 +26,66 @@ double Integer::getValue()
 {
 	return inputNum;
 }
+
+
+void Integer::add(Number num)
+{
+	cout << "Successfully recognized adding a generic Number to this Integer" << endl; //all outputs here are for testing only
+	// Do nothing
+}
+
+int Integer::add(Integer num)
+{
+	// needs to return answer, figure out format first. May affect above line (answer may need to be Integer).
+	// same goes for other implemented methods.
+	int answer = this->getValue() + num.getValue(); //works
+	return answer; 
+}
+
+void Integer::subtract(Number num)
+{
+	cout << "Successfully recognized subtracting a generic Number from this Integer" << endl;
+	// Do nothing
+}
+
+int Integer::subtract(Integer num)
+{
+	int answer = this->getValue() - num.getValue(); //works
+	return answer;
+}
+
+void Integer::multiply(Number num)
+{
+	cout << "Successfully recognized multiplying a generic Number and this Integer" << endl;
+	// if coefficient == 1, do nothing
+	// else multiply coefficient with Integer
+}
+
+int Integer::multiply(Integer num)
+{
+	int answer = (this->getValue()) * (num.getValue()); //works
+	return answer;	
+
+}
+
+void Integer::divide(Number num)
+{
+	cout << "Successfully recognized dividing this Integer by a generic Number. Or vice versa" << endl;
+}
+
+/*
+Rational Integer::divide(Integer num)
+{
+	// This implementation assumes 'num' is the divisor
+
+	double answer = this->getValue() / num.getValue(); //works
+	Rational r = new Rational(answer);
+	return r;
+
+	// Alternative implementation: returning a Rational:
+
+//	Rational *ratAnswer = new Rational(getInt(), num.getInt());
+//	cout << ratAnswer->getNum() << "/" << ratAnswer->getDen() << endl;
+}
+*/
+
--- src/Integer.h
+++ src/Integer.h
@@ -11,7 +11,7 @@
 
 #include <iostream>
 #include "Number.h"
-
+#include "Rational.h"
 using namespace std;
 
 class Integer : public Number
@@ -22,8 +22,23 @@ private:
     
 public:
 	Integer(); // default constructor for testing
-	Integer(int);
+	Integer(int inputNum);
+	
+	// TODO: need to implement add, subtract, etc. How do we implement this dependent upon Number subclass?
+	void add(Number n);
+	int add(Integer z);
+
+	void subtract(Number n );
+	int subtract(Integer z);
+
+	void multiply(Number n );
+	int multiply(Integer z);
+
+	void divide(Number n);
+//	Rational divide(Integer z);
+
 	double getValue();
+    
 };
 
 #endif /* defined(__COP_3503_Project__Integer__) */
