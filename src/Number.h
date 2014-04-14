406fab4fcdb79bc7fed47dcc8fa7ecd564c039ed
Justin Bisignano <justinbis@fake.server>
2014-04-14 01:20:20 -0400

    Cleaned up Number

--- src/Number.h
+++ src/Number.h
@@ -18,14 +18,7 @@ using namespace std;
 
 class Number
 {
-public:
-		// Commented out for working purposes.
-		//We can't create objects that don't define all of these functions and we need to for testing.
-		//virtual void add(Number num) = 0; // void since actual returns should be subclasses
-		//virtual void subtract() = 0;
-		//virtual void multiply() = 0;
-		//virtual void divide() = 0;
-	
+public:	
 	virtual double getValue();
 	virtual string getType();
 	
